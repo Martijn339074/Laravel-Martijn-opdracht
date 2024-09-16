8b20ff0326d7bf48b2ed6fe9e11eeb0c331f668b
Martijn339074 <339074@student.mboutrecht.nl>
2024-09-16 11:57:17 +0200

    bezig met filmpje 6

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100 bg-gray-100">
 
 <head>
     <meta charset="UTF-8">
@@ -20,7 +20,7 @@
     <div class="min-h-full">
         <nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div class="flex h-16 items-center justify-between">
+                <div class="flex h-16    items-center justify-between">
                     <div class="flex items-center">
                         <div class="flex-shrink-0">
                             <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
@@ -28,11 +28,11 @@
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                                <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                                <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                                <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+                                <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
+                                <x-nav-link href="/contact" :active="request()->is('contact')" >Contact</x-nav-link>
                             </div>
-                        </div>
+                        </div>                        
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-4 flex items-center md:ml-6">
--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,25 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        "jobs" => [
+            [
+                "title" => "Director",
+                "salary" => "$50,000"
+            ],
+            [
+                "title" => "Programmer",
+                "salary" => "$20,000"
+            ],
+            [
+                "title" => "Teacher",
+                "salary" => "$40,000"
+            ]
+        ]
+    ]);
 });
 
 Route::get('/contact', function () {
     return view('contact');
 });
-
