e76f40d408c28953a05fb1e15fc10d2c52f69490
Martijn339074 <339074@student.mboutrecht.nl>
2024-10-04 14:14:26 +0200

    filmje 12 klaar

--- app/Models/job.php
+++ app/Models/job.php
@@ -12,4 +12,15 @@ class Job extends Model {
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer() 
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tag() 
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
+
