e9d8e736f9ee71a418d9af65a5dff38b0cd935e4
Martijn339074 <339074@student.mboutrecht.nl>
2024-10-07 11:46:57 +0200

    filmje 14 klaar

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,14 +3,22 @@
         Jobs Page
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach ($jobs as $job)
         <li>
-            <a href="/jobs/{{ $job['id']}}">
-            <strong>{{ $job['title']}}</strong>: Pays {{$job['salary']}} per year
+            <a href="/jobs/{{ $job['id']}}" class="block px-4 py-6 border border-gray-200">
+                <div class="font-bold font-blue text-smcomposer require barryvdh/laravel-debugbar --dev">
+                    {{ $job->employer->name}}
+                </div>
+                <div>
+                    <strong>{{ $job['title']}}</strong>: Pays {{$job['salary']}} per year
+                </div>
             </a>
         </li>
         @endforeach
-    </ul>
+        <div>
+            {{$jobs->links()}}
+        </div>
+    </div>
 
 </x-layout>
